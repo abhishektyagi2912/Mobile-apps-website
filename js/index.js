f2d14a1da46aec0dbd3a504f020176ab002821f8
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-12-12 23:23:26 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+
+gsap.to(".fleftelem", {
+    scrollTrigger: {
+        trigger: "#featuredimages",
+        pin: true,
+        start: "top top",
+        end: "bottom bottom",
+        endTrigger: ".last",
+        scrub: 1,
+    },
+    y: "-300%",
+    ease: Power1
+});
+
+let sections = document.querySelectorAll(".fleftelem");
+Shery.imageEffect(".images", {
+    style: 5,
+    config:{onMouse:{value:1}},
+    slideStyle: (setScroll) => {
+        sections.forEach(function (section, index) {
+            ScrollTrigger.create({
+                trigger: section,
+                start: "top top",
+                scrub: 1,
+                onUpdate: function (prog) {
+                    setScroll(prog.progress + index);
+                },
+            });
+        });
+    },
+});
