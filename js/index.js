313f560cded4a4401547efcf29c1cc495aac5101
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-12-14 18:55:29 +0530

    handle the mobile opening

--- js/index.js
+++ js/index.js
@@ -36,44 +36,18 @@ Shery.imageEffect(".images", {
         });
     },
 });
-// function locomotive() {
-//     gsap.registerPlugin(ScrollTrigger);
+document.addEventListener("DOMContentLoaded", function () {
+    var mobileWarning = document.getElementById("mobile-warning");
 
-//     // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-//     const locoScroll = new LocomotiveScroll({
-//         el: document.querySelector(".smooth-scroll"),
-//         smooth: true
-//     });
+    // Check if the user is on a mobile device
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        mobileWarning.style.display = "block";
 
-//     // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-//     locoScroll.on("scroll", () => {
-//         ScrollTrigger.update();
-//     });
-
-//     // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-//     ScrollTrigger.scrollerProxy(".smooth-scroll", {
-//         scrollTop(value) {
-//             return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//         },
-//         getBoundingClientRect() {
-//             return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-//         },
-//         // LocomotiveScroll handles things completely differently on mobile devices
-//         pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
-//     });
-
-//     // Refresh ScrollTrigger when the window is resized
-//     window.addEventListener("resize", () => {
-//         locoScroll.update();
-//         ScrollTrigger.refresh();
-//     });
-
-//     // Refresh ScrollTrigger when content is added or removed dynamically
-//     locoScroll.on("call", (value, way, obj) => {
-//         if (value === "refreshScrollTrigger") {
-//             ScrollTrigger.refresh();
-//         }
-//     });
-// }
-
-// locomotive();
+        var contentElements = document.querySelectorAll("#main, #footer");
+        contentElements.forEach(function (element) {
+            element.style.display = "none";
+        });
+    } else {
+        mobileWarning.style.display = "none";
+    }
+});
